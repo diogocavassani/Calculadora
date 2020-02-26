8f5ae717778dea87674beeb543e881126a03ba80
Diogo Cavassani <diogocavassani@gmail.com>
2020-02-26 20:43:53 -0300

    Calculadora

---
+++ src/Calculadora.WebAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Calculadora.WebAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ src/Calculadora.WebAppMVC/wwwroot/js/site.js
@@ -0,0 +1,72 @@
+﻿$(document).ready(function () {
+    var agregado = 0;
+    var valorTotal = 0;
+    var valorOperacao = "";
+
+    $(".btn-numero").click(function () {
+        let valorVisor = $("#visorCalculadora").val();
+        valorVisor += $(this).html();
+
+        $("#visorCalculadora").val(valorVisor);
+    });
+
+    $(".btn-operacao").click(function () {
+        let operacao = $(this).html();
+        let valorVisor = parseInt($("#visorCalculadora").val());
+        
+
+        switch (operacao) {
+            case "+":
+                agregado = agregado == 0 ? valorVisor : agregado + valorVisor;
+                valorOperacao = "+";
+                $("#visorCalculadora").val("");
+                break;
+            case "-":
+                agregado = agregado == 0 ? valorVisor : agregado - valorVisor;
+                $("#visorCalculadora").val("");
+                valorOperacao = "-";
+                break;
+            case "x":
+                agregado = agregado == 0 ? valorVisor : agregado * valorVisor;
+                valorOperacao = "X";
+                $("#visorCalculadora").val("");
+                break;
+            case "/":
+                agregado = agregado == 0 ? valorVisor : agregado / valorVisor;
+                valorOperacao = "/";
+                $("#visorCalculadora").val("");
+                break;
+            case "=":
+                if (valorOperacao == "+") {
+                    valorTotal = agregado + valorVisor;
+                    $("#visorCalculadora").val(valorTotal);
+                } 
+                
+                if (valorOperacao == "-"){
+                    valorTotal = agregado - valorVisor;
+                    $("#visorCalculadora").val(valorTotal);
+                } 
+                
+                if (valorOperacao == "X") {
+                    valorTotal = agregado * valorVisor;
+                    $("#visorCalculadora").val(valorTotal);
+                }
+                
+                if (valorOperacao == "/") {
+                    valorTotal = agregado / valorVisor;
+                    $("#visorCalculadora").val(valorTotal);
+                }
+                else 
+                break;
+                agregado = 0;
+                break;
+            default:
+                $("#visorCalculadora").val("");
+                agregado = 0;
+                valorVisor = 0;
+                valorTotal = 0;
+                valorOperacao = ""
+                break;
+        }
+    });
+});
